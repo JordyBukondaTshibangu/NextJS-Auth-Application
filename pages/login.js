3ff2efff12d6bdee609eefa9f0a5168b9a9c3205
jordy_tshibss <jordytshibss@gmail.com>
2023-03-01 13:33:01 +0200

    style : login page styling

--- pages/login.js
+++ pages/login.js
@@ -1,42 +1,67 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import LoginImage from "../public/assets/undraw_my_password_re_ydq7.svg";
+import google from "../public/assets/google.webp";
 
 const Login = () => {
   const { data: session } = useSession();
 
-  console.log(session);
   return (
-    <main className="bg-[#F5F5DC] h-full min-h-screen">
-      <section className="max-w-7xl container mx-auto bg-grey-800 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold leading-7 pt-20">Please login</h1>
-        <form className="w-[30%] bg-white flex flex-col gap-5 mt-20 border border-grey-400 p-5 rounded-md pt-10">
+    <main className="bg-[#63FFB9] h-full min-h-screen">
+      <section className="max-w-7xl  container mx-auto min-h-screen bg-grey-800 flex items-center justify-center gap-12">
+        <form className="w-1/3 h-full bg-white rounded-xl shadow-2xl flex flex-col gap-5 px-10">
+          <div>
+            <h1 className="text-4xl font-bold leading-7 pt-20 text-[#000080]">
+              Login
+            </h1>
+            <p className="text-gray-400 text-sm font-medium mt-5">
+              Doesn't have an account yet ?{" "}
+              <span className="font-bold">Sign up</span>
+            </p>
+          </div>
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
-              <label className="text-sm font-medium">Email</label>
+              <label className="text-gray-400 text-lg">Email Address</label>
               <input
                 type="email"
-                className="h-[45px] w-full border-0 border-b-2 outline-none"
+                placeholder="your@example.com"
+                className="h-[45px] w-full border-gray-400 border-2 rounded-lg px-5"
               />
             </div>
             <div className="flex flex-col gap-2">
-              <label className="text-sm font-medium">Password</label>
+              <label className="text-gray-400 text-lg">Password</label>
               <input
-                type="pasword"
-                className="h-[45px] w-full border-0 border-b-2 outline-none"
+                type="password"
+                placeholder="Enter 6 characters or more"
+                className="h-[45px] w-full border-gray-400 border-2 rounded-lg px-5"
               />
             </div>
-            <div className="flex justify-between">
-              <button className="bg-blue-300 flex items-center justify-center p-3 mt-10 rounded-md text-white text-xl">
-                Login
-              </button>
+            <div className="flex gap-3">
+              <input
+                type="checkbox"
+                placeholder="Enter 6 characters or more"
+                className="h-[15px] border-gray-400 border-2 rounded-lg"
+              />
+              <label className="text-gray-400 text-xs">Remember Me</label>
+            </div>
+            <button className="bg-blue-500 flex items-center justify-center p-3 mt-5 rounded-md text-white text-xl">
+              Login
+            </button>
+            <p className="text-center text-gray-400 text-sm">Or login with</p>
+            <div className="flex mb-10">
               <button
-                className="flex items-center justify-center p-1 mt-10 rounded-md underline text-xs"
+                className="flex items-center justify-center gap-5 p-3 mt-5 rounded-md text-gray-400 text-sm border-2 border-red-200"
                 onClick={() => signIn()}
               >
-                Login with Google
+                <Image src={google} width={25} height={25} />
+                <span className="text-red-400">Login with Google</span>
               </button>
             </div>
           </div>
         </form>
+        <div className="bg-red-100 rounded-full">
+          <Image src={LoginImage} width={600} height={600} />
+        </div>
       </section>
     </main>
   );
